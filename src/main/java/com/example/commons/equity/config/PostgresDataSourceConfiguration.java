8821742880055a4db59e9c95992a0118fc95e935
tymofieievv <vladyslav.t@itsprodigy.com>
2022-11-17 10:13:16 +0100

    Postgres configuration

---
+++ src/main/java/com/example/commons/equity/config/PostgresDataSourceConfiguration.java
@@ -0,0 +1,62 @@
+package com.example.commons.equity.config;
+
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class PostgresDataSourceConfiguration {
+
+//    @Bean
+//    @Qualifier("postgresDataSource")
+//    public DataSource getDataSource() {
+//        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+//        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/equity");
+//        dataSourceBuilder.username("postgres");
+//        dataSourceBuilder.password("123456");
+//        return dataSourceBuilder.build();
+//    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(false);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+
+        factory.setPackagesToScan("com.example.commons.equity.model.entity");
+        factory.setDataSource(dataSource);
+        factory.setJpaPropertyMap(jpaProperties());
+        return factory;
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+
+    protected Map<String, Object> jpaProperties() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("hibernate.physical_naming_strategy", SpringPhysicalNamingStrategy.class.getName());
+        props.put("hibernate.implicit_naming_strategy", SpringImplicitNamingStrategy.class.getName());
+        return props;
+    }
+}
